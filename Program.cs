caba25d99e96ac5c16f6482b197a31857045ade8
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-06-19 21:49:10 -0500

    fixed typo

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace simple_network {
     public class Program {
         public static void Main(String[] args) {
-            HashSet<DataPoint> dataPoints = CSVParser.parse("./data/my_madeup_dataset.csv",2,2);
+            HashSet<DataPoint> dataPoints = CSVParser.Parse("./data/my_madeup_dataset.csv",2,2);
             
             using (var window = new Visualize(800, 800, "OpenTK Shaders"))
             {
